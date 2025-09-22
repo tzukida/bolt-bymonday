f3dc478c0bc035c01668bbd198ca0cc9b8c00aab
tzukida <168533020+tzukida@users.noreply.github.com>
2025-09-23 01:54:50 +0800

    Updated DataContext.tsx

--- contexts/DataContext.tsx
+++ contexts/DataContext.tsx
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useEffect, useState } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useAuth } from '@/AuthContext';
+import { useAuth } from './AuthContext';
 import { supabaseService } from '@/services/supabaseService';
 import { APP_CONFIG } from '@/config/app';
 
