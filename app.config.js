9f108f98ced1a7d7d5fa091a0839d3bdc66ee2ef
tzukida <168533020+tzukida@users.noreply.github.com>
2025-09-23 02:17:36 +0800

    Updated app.config.js

---
+++ app.config.js
@@ -0,0 +1,30 @@
+import 'dotenv/config'; // This requires the dotenv package to be installed
+
+export default {
+  expo: {
+    name: "bolt-expo-nativewind",
+    slug: "bolt-expo-nativewind",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "myapp",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true
+    },
+    web: {
+      bundler: "metro",
+      output: "single",
+      favicon: "./assets/images/favicon.png"
+    },
+    plugins: ["expo-router", "expo-font", "expo-web-browser"],
+    experiments: {
+      typedRoutes: true
+    },
+    extra: {
+      supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL,
+      supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
+    },
+  },
+};
